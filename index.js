6353a19c79c0ffa9c220b8485ab28120c0691825
Florian Keller <mail@floriankeller.de>
2015-10-25 10:43:04 +0100

    Accept commands with and without arguments

--- index.js
+++ index.js
@@ -10,21 +10,37 @@ var bot = new TelegramBot(token, {
 });
 
 bot.onText(/\/.*/, function(msg) {
-	var regex = /\/([^ ]+) (.*)/;
-		commandwithVariable = msg.text.match(regex),
-		console.log(commandwithVariable);
-		command = commandwithVariable[1],
-		string = commandwithVariable[2];
-	console.log("message received: " + msg.text);
-	console.log("command received: " + command);
-	switch (command) {
-		case "unsubscribe":
-			return bot.sendMessage(msg.from.id, "unsubscribed: \"" + string + "\"");
-			break;
-		default:
-			return bot.sendMessage(msg.from.id, "command: " + command);
-			break;
-	}
+	var cmdArg = msg.text.match(/\/([^ ]+) (.*)/);
+		cmdNoArg = msg.text.match(/\/(.*)/);
+
+		if (cmdArg) {
+			var cmd = cmdArg[1],
+				string = cmdArg[2];
+
+			console.log(cmdArg);
+
+			switch (cmd) {
+				case "subscribe":
+					return bot.sendMessage(msg.from.id, "unsubscribed: \"" + string + "\"");
+					break;
+				case "unsubscribe":
+					return bot.sendMessage(msg.from.id, "unsubscribed: \"" + string + "\"");
+					break;
+				default:
+					return bot.sendMessage(msg.from.id, "unknown command!");
+					break;
+			}
+		} else if (cmdNoArg) {
+			var cmd = cmdNoArg[1];
+
+			switch (cmd) {
+				case "subscribe":
+					return bot.sendMessage(msg.from.id, "please provide a ticket number");
+					break;
+				default:
+					return bot.sendMessage(msg.from.id, "unknown command!");
+			}
+		}
 });
 
 console.log("Bot is running");
