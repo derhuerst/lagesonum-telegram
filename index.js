9773d40c9550d83efc7ba045a93829eeb7aa3585
Florian Keller <mail@floriankeller.de>
2015-10-25 12:26:18 +0100

    Better command handling

--- index.js
+++ index.js
@@ -1,46 +1,57 @@
 // index.js
 
 var	config = require('config'),
+	shortid = require('shortid'),
+	request = require('request'),
 	TelegramBot = require('node-telegram-bot-api'),
-	token = config.telegramToken,
-	request = {};
+	token = config.telegramToken;
+
+var request = {
+		"requestid": "",
+		"language": "de_DE",
+		"tickets": [],
+		"subscribe": false
+	};
 
 var bot = new TelegramBot(token, {
 	polling: true
 });
 
-bot.onText(/\/.*/, function(msg) {
-	var cmdArg = msg.text.match(/\/([^ ]+) (.*)/);
-		cmdNoArg = msg.text.match(/\/(.*)/);
+bot.onText(/\/subscribe (.+)/, function(msg, match) {
+	var arg = match[1];
 
-		if (cmdArg) {
-			var cmd = cmdArg[1],
-				string = cmdArg[2];
-
-			console.log(cmdArg);
-
-			switch (cmd) {
-				case "subscribe":
-					return bot.sendMessage(msg.from.id, "unsubscribed: \"" + string + "\"");
-					break;
-				case "unsubscribe":
-					return bot.sendMessage(msg.from.id, "unsubscribed: \"" + string + "\"");
-					break;
-				default:
-					return bot.sendMessage(msg.from.id, "unknown command!");
-					break;
-			}
-		} else if (cmdNoArg) {
-			var cmd = cmdNoArg[1];
-
-			switch (cmd) {
-				case "subscribe":
-					return bot.sendMessage(msg.from.id, "please provide a ticket number");
-					break;
-				default:
-					return bot.sendMessage(msg.from.id, "unknown command!");
-			}
-		}
+	if (checkTicket(arg)) {
+		request.requestid = shortid.generate();
+		request.subscribe = true;
+		request.tickets.push(arg);
+		console.log(request);
+		// send request
+		return bot.sendMessage(msg.from.id, "Successfully subscribed to ticket: \"" + arg + "\"");
+	} else {
+		return bot.sendMessage(msg.from.id, "Wrong ticket format. Tickets have this format: B123");
+	}
 });
+
+bot.onText(/\/unsubscribe (.+)/, function(msg, match) {
+	var arg = match[1];
+
+	if (checkTicket(arg)) {
+		request.requestid = shortid.generate();
+		request.tickets.push(arg);
+		console.log(request);
+		// send request
+		return bot.sendMessage(msg.from.id, "Successfully unsubscribed from ticket: \"" + arg + "\"");
+	} else {
+		return bot.sendMessage(msg.from.id, "Wrong ticket format. Tickets have this format: B123");
+	}
+});
+
+bot.onText(/\/(?!subscribe|unsubscribe)[a-z0-9]+/, function(msg, match) {
+	return bot.sendMessage(msg.from.id, "unknown command!"); // + usage
+});
+
+function checkTicket(arg) {
+	return arg.match(/[A-Za-z][0-9]{3}/);
+}
 
 console.log("Bot is running");
