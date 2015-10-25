76c9a94a8c533ab24996306cec197056fdebf030
Jannis R <mail@jannisr.de>
2015-10-25 16:23:58 +0100

    new LaGeSoNum dev hostname

--- config/default.js
+++ config/default.js
@@ -3,7 +3,7 @@
 module.exports = {
 	telegramToken:	'140520703:AAGBRXPvz2-t15MMqWmnQ0D-dG6MVE1ixfU',
 	lagesonum: {
-		hostname:	'cookie0.de',
+		hostname:	'localhost',
 		port:		80,
 		pathname:	'/post.php'
 	}
